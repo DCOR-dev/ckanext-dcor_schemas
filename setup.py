0f24577d277954b24429dde99a100b7fa0fd44bb
Paul Müller <dev@craban.de>
2020-08-31 13:55:53 +0200

    cleanup: there is not dependency, mime type is from file extension.

--- setup.py
+++ setup.py
@@ -27,10 +27,10 @@ setup(
     package_dir={name: name},
     namespace_packages=['ckanext'],
     install_requires=[
-      # CKAN extensions should not list dependencies here, but in a separate
-      # ``requirements.txt`` file.
-      #
-      # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
+        # CKAN extensions should not list dependencies here, but in a separate
+        # ``requirements.txt`` file.
+        #
+        # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
     ],
     include_package_data=True,
     # To provide executable scripts, use entry points in preference to the
