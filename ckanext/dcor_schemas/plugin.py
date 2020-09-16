fc1a62ecdf67cf2c5025a3d891571c4024a1cb85
Paul Müller <dev@craban.de>
2020-09-16 10:36:57 +0200

    maintenance release

--- ckanext/dcor_schemas/plugin.py
+++ ckanext/dcor_schemas/plugin.py
@@ -74,7 +74,7 @@ class DCORDatasetFormPlugin(plugins.SingletonPlugin,
             toolkit.get_action('config_option_update')(
                 context={'ignore_auth': True, 'user': None},
                 data_dict={'licenses_group_url': license_loc}
-                )
+            )
 
     def update_config_schema(self, schema):
         ignore_missing = toolkit.get_validator('ignore_missing')
