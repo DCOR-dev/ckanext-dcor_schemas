6dfedba006e1481e47f11f4a8820389b8a71727c
Paul Müller <dev@craban.de>
2021-02-19 16:19:03 +0100

    ci: use dcor-test 0.1.0 vagrant image

--- vagrant-run-tests.sh
+++ vagrant-run-tests.sh
@@ -6,9 +6,7 @@ export CKAN_INI=/etc/ckan/default/test-dcor.ini
 cd "$(dirname "${BASH_SOURCE[0]}")"
 # Install the package in editable mode for testing
 source /usr/lib/ckan/default/bin/activate
-ckan db init
-pip install codecov coverage
-pip install pytest-ckan
 pip install -e .
 # run tests
-coverage run -m pytest --disable-warnings ckanext
+coverage run -m pytest -p no:warnings ckanext
+codecov
