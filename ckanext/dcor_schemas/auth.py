610411a90db3afb5cf49f9f0cd078014262dc4d5
Paul Müller <dev@craban.de>
2025-09-17 08:32:56 +0200

    fix: deny user listing for non-admins

--- ckanext/dcor_schemas/auth.py
+++ ckanext/dcor_schemas/auth.py
@@ -536,73 +536,31 @@ def user_create(context, data_dict=None):
     return {'success': True}
 
 
-# User authentication methods copied from
-# https://github.com/qld-gov-au/ckanext-qgov
-def _has_user_permission_for_some_group(user_name, permission):
-    """Check if the user has the given permission for any group.
-    """
-    user_id = authz.get_user_id_for_username(user_name, allow_none=True)
-    if not user_id:
-        return False
-    roles = authz.get_roles_with_permission(permission)
-
-    if not roles:
-        return False
-    # get any groups the user has with the needed role
-    q = model.Session.query(model.Member) \
-        .filter(model.Member.table_name == 'user') \
-        .filter(model.Member.state == 'active') \
-        .filter(model.Member.capacity.in_(roles)) \
-        .filter(model.Member.table_id == user_id)
-    group_ids = []
-    for row in q.all():
-        group_ids.append(row.group_id)
-    # if not in any groups has no permissions
-    if not group_ids:
-        return False
-
-    # see if any of the groups are active
-    q = model.Session.query(model.Group) \
-        .filter(model.Group.state == 'active') \
-        .filter(model.Group.id.in_(group_ids))
-
-    return bool(q.count())
-
-
-def _requester_is_admin(context):
-    """Check whether the current user has admin privileges in some group
-    or organisation.
-    This is based on the 'update' privilege; see eg
-    ckan.logic.auth.update.group_edit_permissions.
-    """
-    requester = context.get('user')
-    return _has_user_permission_for_some_group(requester, 'admin')
-
-
 def user_list(context, data_dict=None):
     """Check whether access to the user list is authorised.
-    Restricted to organisation admins as per QOL-5710.
+
+    Restricted to site admins.
     """
-    return {'success': _requester_is_admin(context)}
+    return {"success": False,
+            "msg": "Listing users is forbidden."}
 
 
 @logic.auth_allow_anonymous_access
 def user_show(context, data_dict):
     """Check whether access to individual user details is authorised.
-    Restricted to organisation admins or self, as per QOL-5710.
+    Restricted to site admins or self
     """
-    if _requester_is_admin(context):
-        return {'success': True}
     requester = context.get('user')
-    id = data_dict.get('id', None)
-    if id:
-        user_obj = model.User.get(id)
+    user_id = data_dict.get('id', None)
+    if user_id:
+        user_obj = model.User.get(user_id)
     else:
         user_obj = data_dict.get('user_obj', None)
     if user_obj:
         return {'success': requester in [user_obj.name, user_obj.id]}
 
-    return {'success': False}
+    return {'success': False,
+            'msg': "Users may only view their own details"}
 
 
 @logic.auth_allow_anonymous_access
